63205c07f0356c625a8b24a701796a103df7e90b
MaleeqOlan <C23435852@mytudublin.ie>
2025-04-02 14:50:57 +0100

    WeaponWheelUI

---
+++ Assets/WeaponUISelector.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUISelector : MonoBehaviour
+{
+    public Image[] weaponImages; // Assign UI images in the inspector
+    private int currentWeaponIndex = 0;
+
+    void Start()
+    {
+        currentWeaponIndex = PlayerPrefs.GetInt("currentWeapon", 0);
+        UpdateWeaponUI();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeWeapon(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeWeapon(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeWeapon(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeWeapon(3);
+        if (Input.GetKeyDown(KeyCode.Alpha5)) ChangeWeapon(4);
+    }
+
+    void ChangeWeapon(int newIndex)
+    {
+        if (newIndex >= 0 && newIndex < weaponImages.Length)
+        {
+            weaponImages[currentWeaponIndex].gameObject.SetActive(false); // Disable previous
+            currentWeaponIndex = newIndex;
+            weaponImages[currentWeaponIndex].gameObject.SetActive(true);  // Enable new
+            PlayerPrefs.SetInt("currentWeapon", currentWeaponIndex);
+        }
+    }
+
+    void UpdateWeaponUI()
+    {
+        for (int i = 0; i < weaponImages.Length; i++)
+        {
+            weaponImages[i].gameObject.SetActive(i == currentWeaponIndex);
+        }
+    }
+}
