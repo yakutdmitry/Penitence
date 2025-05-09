034b22908884a2c7e3495e8fcf08a18543a8917f
Vienbohol2 <C23786399@mytudublin.ie>
2025-05-09 02:10:38 +0100

    Gatling Syringe Gun & Weapon Manager Tweak
    Had to change the weapon manager in order for the 3rd weapon to fire. The old WeaponManager wasn't playing the animation because it wasn't wired properly to the UI canvas

--- Assets/Scripts/CIanScripts/WeaponManager.cs
+++ Assets/Scripts/CIanScripts/WeaponManager.cs
@@ -3,91 +3,78 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
-    public GameObject[] weaponPrefabs; // weapon array
-    private BaseWeapon currentWeapon; // 
-    public Camera playerCamera;
-    public GameObject crossbowUI;
-    public GameObject pistolUI;
-    public GameObject SyringerUI;
+    [Header("Weapon Prefabs")]
+    public GameObject[] weaponPrefabs;  // 0 = Pistol, 1 = Crossbow, 2 = Syringer
 
-    private SpriteAnimatorUI currentWeaponAnimator; 
+    [Header("References")]
+    public Camera playerCamera;
+
+    [Header("Weapon UI Canvases")]
+    public GameObject pistolUI;
+    public GameObject crossbowUI;
+    public GameObject syringerUI;
+
+    private BaseWeapon currentWeapon;
+    private SpriteAnimatorUI currentWeaponAnimator;
 
     void Start()
     {
-        EquipWeapon(0); 
+        EquipWeapon(0);
     }
 
     void Update()
     {
- 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            EquipWeapon(0); // pistol, its on 1
-            pistolUI.SetActive(true);
-            crossbowUI.SetActive(false);
-            SyringerUI.SetActive(false);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            EquipWeapon(1); // crossbow, on 2
-            pistolUI.SetActive(false);
-            crossbowUI.SetActive(true);
-            SyringerUI.SetActive(false);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            EquipWeapon(2); //Syringer on 3
-            SyringerUI.SetActive(true);
-            pistolUI.SetActive(false);
-            crossbowUI.SetActive(false);
-        }
+        // Weapon switch input
+        if (Input.GetKeyDown(KeyCode.Alpha1)) EquipWeapon(0);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) EquipWeapon(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) EquipWeapon(2);
 
-
-        if (Input.GetButton("Fire1") && currentWeapon != null)
+        // Fire and animate
+        if (Input.GetButtonDown("Fire1") && currentWeapon != null)
         {
-            currentWeapon.Fire(); 
-            // animation for the weapon (trigger)
-            if (currentWeaponAnimator != null)
-            {
-                currentWeaponAnimator.PlayShootAnimation();
-            }
+            currentWeapon.Fire();
+            currentWeaponAnimator?.PlayShootAnimation();
         }
     }
 
     void EquipWeapon(int index)
     {
-        if (index >= 0 && index < weaponPrefabs.Length)
+        if (index < 0 || index >= weaponPrefabs.Length) return;
+
+        // Destroy old weapon
+        if (currentWeapon != null)
+            Destroy(currentWeapon.gameObject);
+
+        // Instantiate new
+        var weaponGO = Instantiate(weaponPrefabs[index], transform.position, transform.rotation);
+        currentWeapon = weaponGO.GetComponent<BaseWeapon>();
+        currentWeapon.playerCamera = playerCamera;
+        currentWeapon.gameObject.SetActive(true);
+
+        // Deactivate all UIs
+        pistolUI.SetActive(false);
+        crossbowUI.SetActive(false);
+        syringerUI.SetActive(false);
+
+        // Activate the one for this weapon and grab its animator
+        switch (index)
         {
-     
-            if (currentWeapon != null)
-            {
-                Destroy(currentWeapon.gameObject);
-            }
-
-            GameObject weaponInstance = Instantiate(weaponPrefabs[index], transform.position, transform.rotation);
-
-
-            currentWeapon = weaponInstance.GetComponent<BaseWeapon>();
-
-            if (currentWeapon != null)
-            {
-                currentWeapon.playerCamera = playerCamera; 
-                currentWeapon.gameObject.SetActive(true);
-            }
-
-            if (index == 0)
-            {
+            case 0:
+                pistolUI.SetActive(true);
                 currentWeaponAnimator = pistolUI.GetComponent<SpriteAnimatorUI>();
-            }
-            else if (index == 1)
-            {
+                break;
+            case 1:
+                crossbowUI.SetActive(true);
                 currentWeaponAnimator = crossbowUI.GetComponent<SpriteAnimatorUI>();
-            }
-            else
-            {
-                //this is where we insert the other weapon anims if needed
+                break;
+            case 2:
+                syringerUI.SetActive(true);
+                currentWeaponAnimator = syringerUI.GetComponent<SpriteAnimatorUI>();
+                break;
+            default:
                 currentWeaponAnimator = null;
-            }
+                break;
         }
     }
 }
+
